618c717b08c63b09e9546e4c80e510b3455527db
Berke Lunstad <bklunstad@gmail.com>
2020-09-11 18:09:10 -0500

    Websocket connection is functional

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-function App() {
-  return (
-    <div className="App">
+const client = new W3CWebSocket('ws://localhost:31001/ws');
+
+class App extends Component {
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }
+  
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <p>
           This is now a messaging service.
         </p>
       </header>
     </div>
-  );
+    );
+  }
 }
+
+/*const App = () => {
+  const socket = new WebSocket('ws://localhost:8080');
+
+
+
+  return (
+    
+  );
+}*/
 
 export default App;
